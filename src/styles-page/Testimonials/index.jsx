aa0757280d81f4a27529cf5beade3138f5675642
Polly <poliana_elen@hotmail.com>
2022-11-29 23:49:32 -0300

    add: Criado um novo componete Twstimonials

--- src/styles-page/Testimonials/index.jsx
+++ src/styles-page/Testimonials/index.jsx
@@ -12,7 +12,26 @@ export default function Testimonials() {
         <div className={styles.box_title}>
           <BoxTitle title={data.title} title_bold={data.title_bold} />
         </div>
-        <div className={styles.box_cards}> </div>
+        <div className={styles.box_cards}>
+          <div className={styles.card}>
+            <picture className={styles.box_img}>
+              <img
+                src="Assets/restaurant/testimonials/image01.png"
+                className={styles.img}
+                alt="Landscape picture"
+              />
+            </picture>
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              diam pellentesque <br /> bibendum non dui volutpat fringilla
+              bibendum. Urna, elit augue urna, vitae feugiat <br /> pretium
+              donec id elementum. Ultrices mattis sed vitae mus risus. Lacus
+              nisi, et ac <br /> dapibus sit eu velit in consequat.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
