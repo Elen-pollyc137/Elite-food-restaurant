36d84b15f1f0d220966c1b997f138398a1c35058
Polly <poliana_elen@hotmail.com>
2022-12-07 15:13:59 -0300

    refector: Refatorando  o componete Footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,82 +1,103 @@
-import { IconMobile, IconPhone, IconWhatsApp, IconWrapper } from "./icon";
+import {
+  IconClock,
+  IconMobile,
+  IconPhone,
+  IconSocial,
+  IconWhatsApp,
+  IconWrapper,
+} from "./icon";
 import styles from "./styles.module.scss";
 
 export default function Footer() {
-  if (true) {
-    return (
-      <footer className={styles.container}>
-        <div className={styles.box}>
-          <div className={styles.box_1}>
-            <strong className={styles.title}>Quer falar com a gente?</strong>
-            <div className={styles.box__people}>
-              <IconPhone />
-
-              <span>0800 </span>
-            </div>
-            <div className={styles.box__people}>
-              <IconMobile />
-              <span>Ligações de celular</span>
-            </div>
-            <div className={styles.box__people}>
-              <IconWhatsApp />
-              <span>Atendimento via WhatsApp</span>
-            </div>
-            <strong className={styles.title}>Comercial</strong>
-            <div className={styles.box__people}>
-              <IconWrapper />
-              <span>vendas@food.com.br</span>
-            </div>
-            <strong className={styles.title}>Atendimento a parceiros</strong>
-            <div className={styles.box__people}>
-              <IconWrapper />
-              <span>parceiro@food.com.br</span>
-            </div>
+  return (
+    <footer className={styles.container}>
+      <div className={styles.info_up}>
+        <span>
+          <strong>Still You Need Our Support ?</strong> <br /> Don’t wait make a
+          smart & logical quote here. Its pretty easy.
+        </span>
+        <div className={styles.card_email}>
+          <input type="email" placeholder=" Enter Your Email" />
+          <button>Subscribe Now</button>
+        </div>
+      </div>
+      <div className={styles.box}>
+        <div className={styles.box_1}>
+          <strong className={styles.title}>About Us.</strong>
+          <div className={styles.box__people}>
+            <p>
+              Corporate clients and leisure travelers has been relying on
+              Groundlink for dependab safe, and professional chauffeured car
+              service in major cities across World.
+            </p>
           </div>
-          <div className={styles.box_1}>
-            <strong className={styles.title}>Links úteis</strong>
-            <ul>
-              <a href="#">
-                <a>About News Partners Team Menu Contacts</a>
-              </a>
-            </ul>
-          </div>
-          <div className={styles.box_1}>
-            <strong className={styles.title}>Institucional</strong>
-            <ul>
-              <a href="#">
-                <a>Trabalhe com a gente</a>
-              </a>
-              <a href="#">
-                <a>Preferências de Cookies</a>
-              </a>
-              <a href="#">
-                <a>Portal de privacidade</a>
-              </a>
-            </ul>
-
-            <picture className={styles.stamp}>
-              <img alt="" src="/Assets/footer/stamp.png" />
-            </picture>
+          <div className={styles.icon_our}>
+            <div>
+              <IconClock />
+            </div>
+            <div>
+              <p>
+                Opening Houres <br />
+                Mon - Sat(8.00 - 6.00) <br /> Sunday - Closed{" "}
+              </p>
+            </div>
           </div>
         </div>
+        <div className={styles.box_1}>
+          <strong className={styles.title}>Useful Links</strong>
+          <ul>
+            <a href="#">About</a>
+            <a href="#">News</a>
+            <a href="#">Partners</a>
+            <a href="#">Team</a>
+            <a href="#">Menu</a>
+            <a href="#">Contacts</a>
+          </ul>
+        </div>
+        <div className={styles.box_1}>
+          <strong className={styles.title}>Help?</strong>
+          <ul>
+            <a href="#">FAQ</a>
+            <a href="#">Term & conditions</a>
+            <a href="#">Reporting</a>
+            <a href="#">Documentation</a>
+            <a href="#">Support</a>
+            <a href="#">Policy Privacy</a>
+          </ul>
+        </div>
+        <div className={styles.box_1}>
+          <strong className={styles.title}>Recent Post</strong>
+          <ul>
+            <picture className={styles.stamp}>
+              <img alt="" src="Assets/restaurant/footer/image01.png" />
+              <p>
+                <strong>20 Feb 2022</strong> <br /> Keep Your Business{" "}
+              </p>
+            </picture>
+            <picture className={styles.stamp}>
+              <img alt="" src="Assets/restaurant/footer/image02.png" />
+              <p>
+                <strong>20 Feb 2022</strong> <br /> Keep Your Business{" "}
+              </p>
+            </picture>
+            <picture className={styles.stamp}>
+              <img alt="" src="Assets/restaurant/footer/image03.png" />
+              <p>
+                <strong>20 Feb 2022</strong> <br /> Keep Your Business{" "}
+              </p>
+            </picture>
+          </ul>
+        </div>
+      </div>
 
-        <div className={styles.info}>
+      <div className={styles.info}>
+        <div>
           <span>
             <strong>© 2022 Food.</strong> Todos os direitos reservados.
           </span>
-          Logo
+          <IconSocial />
         </div>
-      </footer>
-    );
-  } else {
-    return (
-      <footer className={styles.container}>
-        <div className={styles.info}>
-          <span>
-            <strong>© 2022 Cappta.</strong> Todos os direitos reservados.
-          </span>
-        </div>
-      </footer>
-    );
-  }
+      </div>
+    </footer>
+  );
 }
