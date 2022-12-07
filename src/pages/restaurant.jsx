302efcee9b5defbe07599a7209678ba97579c3ff
Polly <poliana_elen@hotmail.com>
2022-12-07 16:15:30 -0300

    refector: Refatorando  o componete Footer e LastestBlog

---
+++ src/pages/restaurant.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../Components/Header/NavBar";
+import HeaderHeroWhite from "../styles-page/restaurant-page/HeaderHeroWhite";
+
+export default function Restaurant() {
+  return (
+    <>
+      <NavBar />
+      <HeaderHeroWhite />
+    </>
+  );
+}
---
+++ src/styles-page/restaurant-page/HeaderHeroWhite/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+export default function HeaderHeroWhite() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.box}>
+        <h1>Text</h1>
+      </div>
+    </section>
+  );
+}
