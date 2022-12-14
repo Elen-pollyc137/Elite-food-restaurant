9901bf719292032fc77e3970bcb28633bf4636fe
Polly <poliana_elen@hotmail.com>
2022-12-14 13:12:47 -0300

    add: Criado um novo componente CategoryCoffee

--- src/styles-page/coffeeshop/CategoryCoffee/indes.jsx
+++ src/styles-page/coffeeshop/CategoryCoffee/indes.jsx
@@ -1,6 +1,76 @@
 import styles from "./styles.module.scss";
 
 export default function CategoryCoffee() {
+  const Category = [
+    {
+      id: 1,
+      img: "Assets/coffee-shop/coffeecategory/image01.png",
+      title: <>Cappuccino</>,
+      text: (
+        <>
+          Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. Leo
+          augue nibh est nisi.
+        </>
+      ),
+    },
+    {
+      id: 2,
+      img: "Assets/coffee-shop/coffeecategory/image02.png",
+      title: <>Cafe latte</>,
+      text: (
+        <>
+          Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. Leo
+          augue nibh est nisi.
+        </>
+      ),
+    },
+    {
+      id: 3,
+      img: "Assets/coffee-shop/coffeecategory/image03.png",
+      title: <>Dark coffee</>,
+      text: (
+        <>
+          Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. Leo
+          augue nibh est nisi.
+        </>
+      ),
+    },
+  ];
+  const Categoryright = [
+    {
+      id: 1,
+      img: "Assets/coffee-shop/coffeecategory/image04.png",
+      title: <>Turkish coffee</>,
+      text: (
+        <>
+          Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. Leo
+          augue nibh est nisi.
+        </>
+      ),
+    },
+    {
+      id: 2,
+      img: "Assets/coffee-shop/coffeecategory/image05.png",
+      title: <>Pancakes</>,
+      text: (
+        <>
+          Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. Leo
+          augue nibh est nisi.
+        </>
+      ),
+    },
+    {
+      id: 3,
+      img: "Assets/coffee-shop/coffeecategory/image06.png",
+      title: <>Coffee Bag</>,
+      text: (
+        <>
+          Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. Leo
+          augue nibh est nisi.
+        </>
+      ),
+    },
+  ];
   return (
     <section className={styles.container}>
       <div className={styles.box}>
@@ -12,9 +82,33 @@ export default function CategoryCoffee() {
           </p>
         </div>
         <div className={styles.box_right}>
-          <div className={styles.box_cards}> </div>
+          <div className={styles.box_cards}>
+            {Category.map(({ id, img, title, text }) => (
+              <div key={id} className={styles.options}>
+                <div>
+                  <img src={img} alt="" />
+                </div>
+                <div>
+                  <h3>{title}</h3>
+                  <p>{text}</p>
+                </div>
+              </div>
+            ))}
+          </div>
           <div className={styles.box_img}> </div>
-          <div className={styles.box_cards_right}> </div>
+          <div className={styles.box_cards_right}>
+            {Categoryright.map(({ id, img, title, text }) => (
+              <div key={id} className={styles.options}>
+                <div>
+                  <h3>{title}</h3>
+                  <p>{text}</p>{" "}
+                </div>{" "}
+                <div>
+                  <img src={img} alt="" />
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
