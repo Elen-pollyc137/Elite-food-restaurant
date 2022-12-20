454a60e452641c2c8e36192980424598a4cf0873
Polly <poliana_elen@hotmail.com>
2022-12-19 22:07:37 -0300

    add: Criado um novo componente OurShop

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -1,6 +1,11 @@
 import NavBar from "../Components/Header/NavBar";
 import OurShop from "../Components/OurShop";
 import MenuTemplate from "../styles-page/menu/MenuTemplate";
+import { Options1 } from "../Components/data/menutemplate";
+import MeetOurCheef from "../styles-page/MeetOurCheef";
+import Clientsus from "../styles-page/Clientsus";
+import Partners from "../styles-page/restaurant-page/Partners";
+import Footer from "../Components/Footer";
 
 export default function Menu() {
   const data = [
@@ -22,7 +27,29 @@ export default function Menu() {
         dark="dark"
       />
       <OurShop title="Our Menu" link="Menu" />
-      <MenuTemplate title="polly" />
+      <MenuTemplate
+        title="Starter Menu"
+        data={Options1}
+        img="Assets/menu/menutemplate/image01.png"
+      />
+      <MenuTemplate
+        title="Main Course"
+        data={Options1}
+        imgright="Assets/menu/menutemplate/image02.png"
+      />
+      <Clientsus />
+      <MenuTemplate
+        title="Dessert"
+        data={Options1}
+        img="Assets/menu/menutemplate/image03.png"
+      />
+      <MenuTemplate
+        title="Drinks"
+        data={Options1}
+        imgright="Assets/menu/menutemplate/image04.png"
+      />
+      <Partners />
+      <Footer />
     </>
   );
 }
