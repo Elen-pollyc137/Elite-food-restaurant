fbac97694b84bd49179e973a2ed8b7b4ab705537
Polly <poliana_elen@hotmail.com>
2022-12-20 16:18:33 -0300

    add: Criado um novo componente OurShop

--- src/Components/data/menutemplate/index.js
+++ src/Components/data/menutemplate/index.js
@@ -26,3 +26,87 @@ export const Options1 = [
     price: <>35$</>,
   },
 ];
+export const Options2 = [
+  {
+    id: 1,
+    title: <>Alder Grilled Chinook Salmon</>,
+    text: <>Toasted French bread topped with romano, cheddar</>,
+    price: <>32$</>,
+  },
+  {
+    id: 2,
+
+    title: <>Berries and creme tart</>,
+    text: <>Gorgonzola, ricotta, mozzarella, taleggio</>,
+    price: <>43$</>,
+  },
+  {
+    id: 3,
+    title: <>Tormentoso Bush Pizza Pintoage</>,
+    text: <>Ground cumin, avocados, peeled and cubed</>,
+    price: <>14$</>,
+  },
+  {
+    id: 4,
+
+    title: <>Spicy Vegan Potato Curry</>,
+    text: <>Spreadable cream cheese, crumbled blue cheese</>,
+    price: <>35$</>,
+  },
+];
+export const Options3 = [
+  {
+    id: 1,
+    title: <>Alder Grilled Chinook Salmon</>,
+    text: <>Toasted French bread topped with romano, cheddar</>,
+    price: <>32$</>,
+  },
+  {
+    id: 2,
+
+    title: <>Berries and creme tart</>,
+    text: <>Gorgonzola, ricotta, mozzarella, taleggio</>,
+    price: <>43$</>,
+  },
+  {
+    id: 3,
+    title: <>Tormentoso Bush Pizza Pintoage</>,
+    text: <>Ground cumin, avocados, peeled and cubed</>,
+    price: <>14$</>,
+  },
+  {
+    id: 4,
+
+    title: <>Spicy Vegan Potato Curry</>,
+    text: <>Spreadable cream cheese, crumbled blue cheese</>,
+    price: <>35$</>,
+  },
+];
+export const Options4 = [
+  {
+    id: 1,
+    title: <>Alder Grilled Chinook Salmon</>,
+    text: <>Toasted French bread topped with romano, cheddar</>,
+    price: <>32$</>,
+  },
+  {
+    id: 2,
+
+    title: <>Berries and creme tart</>,
+    text: <>Gorgonzola, ricotta, mozzarella, taleggio</>,
+    price: <>43$</>,
+  },
+  {
+    id: 3,
+    title: <>Tormentoso Bush Pizza Pintoage</>,
+    text: <>Ground cumin, avocados, peeled and cubed</>,
+    price: <>14$</>,
+  },
+  {
+    id: 4,
+
+    title: <>Spicy Vegan Potato Curry</>,
+    text: <>Spreadable cream cheese, crumbled blue cheese</>,
+    price: <>35$</>,
+  },
+];
