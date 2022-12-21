315664eb314decc5788644d31631761c237f750f
Polly <poliana_elen@hotmail.com>
2022-12-20 22:37:01 -0300

    add: Criado um novo componente OurShop

---
+++ src/pages/shoplist.jsx
@@ -0,0 +1,20 @@
+import NavBar from "../Components/Header/NavBar";
+import OurShop from "../Components/OurShop";
+
+export default function ShopList() {
+  return (
+    <>
+      <NavBar
+        home="Home"
+        menu="Menu"
+        blog="Blog"
+        page="Pages"
+        about="About"
+        shop="Shop"
+        contact="Contact"
+        dark="dark"
+      />
+      <OurShop title="Our Menu" link="Shop" />
+    </>
+  );
+}
