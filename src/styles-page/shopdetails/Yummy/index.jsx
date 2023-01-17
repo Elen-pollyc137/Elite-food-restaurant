8f910c1094ed6b6bbead034c5f6cf49d27c8653b
Polly <poliana_elen@hotmail.com>
2023-01-17 12:15:59 -0300

    add: Criado um novo componente shopdetails

--- src/styles-page/shopdetails/Yummy/index.jsx
+++ src/styles-page/shopdetails/Yummy/index.jsx
@@ -3,7 +3,21 @@ import styles from "./styles.module.scss";
 export default function Yummy() {
   return (
     <section className={styles.container}>
-      <div className={styles.box}></div>
+      <div className={styles.box}>
+        <div className={styles.box_left}>
+          <picture className={styles.box_image}>
+            <img
+              src="Assets/shopdetails/yummy/image01.png"
+              className={styles.img}
+            />
+          </picture>
+        </div>
+        <div className={styles.box_rigth}>
+          <div className={styles.box_title}> </div>
+          <div className={styles.box_price}> </div>
+          <div className={styles.box_info}> </div>
+        </div>
+      </div>
     </section>
   );
 }
